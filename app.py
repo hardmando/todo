c7955cf2c36b2703cce9a2a10bd816327e4c1de7
hardmando <totonn777@icloud.com>
2024-11-07 12:21:57 -0600

    add and remove tasks

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+import datetime
+
+from flask import Flask, render_template, request, url_for, flash, redirect
 import psycopg2
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c1f5eb607c6ee4009644da33163dca00e36b6783c0245814'
 
 
 #DB connection
 conn = psycopg2.connect(
     host='localhost',
+    port='5433',
     database='postgres',
     user='admin',
     password='admin'
@@ -30,6 +34,7 @@ conn.close()
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
+                            port='5433',
                             database='postgres',
                             user='admin',
                             password='admin')
@@ -39,6 +44,34 @@ def get_db_connection():
 def home():
     conn = get_db_connection()
     cur = conn.cursor()
+    cur.execute('SELECT * FROM tasks')
+    tasks = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('home.html', tasks=tasks)
+
+@app.route('/post_task', methods=['GET', 'POST'])
+def post_task():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    description = request.form['task_desc']
+    cur.execute('INSERT INTO tasks (description, created_at) VALUES (%s, %s);', (description, datetime.datetime.now()))
+    conn.commit()
+
+    cur.execute('SELECT * FROM tasks')
+    tasks = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('home.html', tasks=tasks)
+
+@app.route('/drop_task', methods=['GET', 'POST'])
+def drop_task():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    task_id = request.form['task_id']
+    cur.execute('DELETE FROM tasks WHERE task_id = %s;', (task_id,))
+    conn.commit()
+
     cur.execute('SELECT * FROM tasks')
     tasks = cur.fetchall()
     cur.close()
